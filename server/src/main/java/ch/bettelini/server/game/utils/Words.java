142b4f2127b1945421d8345673c1fc6cbced5dc3
PaoloBettelini <paolo.bettelini@samtrevano.ch>
2021-06-11 19:49:54 +0200

    file system issue

--- server/src/main/java/ch/bettelini/server/game/utils/Words.java
+++ server/src/main/java/ch/bettelini/server/game/utils/Words.java
@@ -1,5 +1,8 @@
 package ch.bettelini.server.game.utils;
 
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -12,7 +15,12 @@ public class Words {
 
 	static {
 		try {
-			Path path = Path.of(Words.class.getResource("/" + FILENAME).toURI());
+			URI uri = Words.class.getResource("/" + FILENAME).toURI();
+			java.util.Map<String, String> env = new java.util.HashMap<>();
+			String[] array = uri.toString().split("!");
+			FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), env);
+			Path path = fs.getPath(array[1]);
+			fs.close();
 			List<String> list = Files.readAllLines(path);
 			words = list.toArray(new String[list.size()]);
 		} catch (Exception e) {
